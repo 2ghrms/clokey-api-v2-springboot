3825e8ac6a440ce2f7cac7b2198d07acc478e1dd
나용준 <dydwnssk99@gmail.com>
2025-07-29 14:55:30 +0900

    [chore/#1] initial setting (#3)
    * chore: PR 리뷰 요청 자동화
    
    * chore: Issue template 추가
    
    * chore: PR Template
    
    * chore: root module build.gradle 설정
    
    * chore: api, batch, infrastructure, common, 그리고 domain 모듈 생성
    
    * [chore/#1] spotless 적용
    
    * [chore/#1] pre-commit과 gradle task설정
    
    * chore: testing pre-commit
    
    * style: spotless
    
    * style: spotless
    
    * fix: git permission 추가
    
    * [chore/#1] trigger review
    
    * [chore/#1] trigger review
    
    * [chore/#1] reviewer trigger try
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>

---
+++ clokey-batch/src/test/java/org/clokey/clokeybatch/ClokeyBatchApplicationTests.java
@@ -0,0 +1,11 @@
+package org.clokey.clokeybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClokeyBatchApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ clokey-infrastructure/src/main/java/org/clokey/clokeyinfrastructure/ClokeyInfrastructureApplication.java
@@ -0,0 +1,12 @@
+package org.clokey.clokeyinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClokeyInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClokeyInfrastructureApplication.class, args);
+    }
+}
