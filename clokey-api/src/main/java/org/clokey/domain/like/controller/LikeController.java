b4328afbe4e53f71857800f28488ca60857574b6
나용준 <dydwnssk99@gmail.com>
2026-01-11 15:23:15 +0900

    [DEPLOY] OIDC Test (#210)
    * feat: PCKE 필드 추가 (#207)
    
    * feat: PCKE 필드 추가
    
    * fix: test 고치기
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>
    
    * [feat/#137] 기록에 좋아요한 유저 목록을 조회 (#138)
    
    * init : 파일 생성
    
    * feat : 좋아요한 유저 조회 구현
    
    * fix : projection 적용, n+1 문제 해결
    
    * chore : 코드 정리
    
    * fix : 코드 정리
    
    * [feat/#206] OIDC pkce 제거 및 딥링크 TEST (#209)
    
    * feat: PCKE 필드 추가
    
    * fix: test 고치기
    
    * Revert "fix: test 고치기"
    
    This reverts commit 91d8c916defddc84819c1e091bbd7fe72becc647.
    
    * Revert "feat: PCKE 필드 추가"
    
    This reverts commit 875aefb5fbcfab1ef6ae92b75717c1d0f9465ed5.
    
    * fix: 딥링크 리다이랙트
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>
    Co-authored-by: paeng <127924700+juuuuone@users.noreply.github.com>

--- clokey-api/src/main/java/org/clokey/domain/like/controller/LikeController.java
+++ clokey-api/src/main/java/org/clokey/domain/like/controller/LikeController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.clokey.code.GlobalBaseSuccessCode;
 import org.clokey.domain.like.dto.response.LikedHistoriesResponse;
+import org.clokey.domain.like.dto.response.LikedMembersResponse;
 import org.clokey.domain.like.service.LikeService;
 import org.clokey.global.annotation.PageSize;
 import org.clokey.response.BaseResponse;
@@ -26,8 +27,27 @@ public class LikeController {
 
     private final LikeService likeService;
 
+    @GetMapping("/users")
+    @Operation(
+            operationId = "Like_getLikedMembers",
+            summary = "좋아요한 유저 조회",
+            description = "내 기록을 좋아요한 유저를 조회합니다")
+    public BaseResponse<SliceResponse<LikedMembersResponse.LikedMemberPreview>> getLikedMembers(
+            @Parameter(description = "기록 ID") @RequestParam Long historyId,
+            @Parameter(description = "이전 페이지의 좋아요 ID (첫 요청 시 생략)") @RequestParam(required = false)
+                    Long lastLikeId,
+            @Parameter(description = "페이지당 조회할 개수") @RequestParam @PageSize Integer size) {
+        SliceResponse<LikedMembersResponse.LikedMemberPreview> response =
+                likeService.getLikedMembers(historyId, lastLikeId, size);
+
+        return BaseResponse.onSuccess(GlobalBaseSuccessCode.OK, response);
+    }
+
     @GetMapping("/histories")
-    @Operation(summary = "좋아요한 기록 조회", description = "사용자가 좋아요한 기록을 조회합니다.")
+    @Operation(
+            operationId = "Like_getLikedHistories",
+            summary = "좋아요한 기록 조회",
+            description = "사용자가 좋아요한 기록을 조회합니다.")
     public BaseResponse<SliceResponse<LikedHistoriesResponse.LikedHistoryPreview>>
             getLikedHistories(
                     @Parameter(description = "이전 페이지의 좋아요 ID (첫 요청 시 생략)")
