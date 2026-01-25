61a2b7ff6ea884574f01f3f3009b0ef861289643
2ghrms <aa020228@gmail.com>
2026-01-24 20:01:37 -0600

    feat : 검색 추천용 커스텀 QueryDSL 레포지토리용 DTO 추가

--- clokey-api/src/main/java/org/clokey/domain/search/repository/RecommendHistoryRow.java
+++ clokey-api/src/main/java/org/clokey/domain/search/repository/RecommendHistoryRow.java
@@ -1,3 +1,3 @@
 package org.clokey.domain.search.repository;
 
-public record RecommendHistoryRow(Long historyId, String subTitle) {}
+public record RecommendHistoryRow(Long historyId, String subTitle, Long memberId) {}
